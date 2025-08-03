d31a8196b0ebe0a620e7f9c3a16a3bc1139db8b6
Oleh Yatsenko <yatsenko.prog@gmail.com>
2025-08-03 20:07:31 +0300

    2-snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,40 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const delay = Number(formData.get('delay'));
+  const state = formData.get('state');
+
+  createPromise(delay, state)
+    .then(ms => {
+      iziToast.success({
+        title: '✅ Success',
+        message: `Fulfilled promise in ${ms}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(ms => {
+      iziToast.error({
+        title: '❌ Error',
+        message: `Rejected promise in ${ms}ms`,
+        position: 'topRight',
+      });
+    });
+
+  form.reset();
+}
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      state === 'fulfilled' ? resolve(delay) : reject(delay);
+    }, delay);
+  });
+}
